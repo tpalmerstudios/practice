47bd02828b56709f683326ae97487045c2374389
Tim Palmer <tpalmerstudios@gmail.com>
2025-12-23 22:35:07 -0600

    enter raw mode

---
+++ kilo/main.c
@@ -0,0 +1,69 @@
+/************************
+ * INCLUDES
+ * *********************/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+/************************
+ * DEFINES
+ * ********************/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+struct termios orig_termios;
+
+void
+die (const char *s)
+{
+	perror (s);
+	exit (1);
+}
+void
+disableRawMode ()
+{
+	if (tcsetattr (STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die ("tcsetattr");
+}
+
+void
+enableRawMode ()
+{
+	if (tcgetattr (STDIN_FILENO, &orig_termios) == -1) die ("tcgetattr");
+	atexit (disableRawMode);
+
+	struct termios raw = orig_termios;
+
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+	raw.c_iflag &= ~(IXON | ICRNL);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if (tcsetattr (STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+		die("tcsetattr");
+}
+
+int
+main (void)
+{
+	enableRawMode ();
+
+	while (1)
+		{
+			char c = '\0';
+			if (read (STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+				die("read");
+			if (iscntrl (c))
+				{
+					printf ("%d\r\n", c);
+				}
+			else
+				{
+					printf ("%d ('%c')\r\n", c, c);
+				}
+			if (c == CTRL_KEY('q')) break;
+		}
+	return 0;
+}
